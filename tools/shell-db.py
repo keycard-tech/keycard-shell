d82bfa062c5d40fb23f0e7e1b6815f1816bfcf76
Michele Balistreri <michele@bitgamma.com>
2025-05-14 10:11:48 +0200

    small refactoring of python tools

--- tools/shell-db.py
+++ tools/shell-db.py
@@ -6,35 +6,9 @@ import struct
 import requests
 
 from common import PAGE_SIZE, WORD_SIZE
+from tokens import *
 
-CHAIN_MAGIC = 0x4348
-ERC20_MAGIC = 0x3020
 VERSION_MAGIC = 0x4532
-
-def serialize_addresses(addresses):
-    res = b''
-    for id, address in addresses.items():
-        if len(address) != 42:
-            assert "Unexpected address format"
-        res = res + struct.pack("<I20s", id, bytes.fromhex(address[2:]))
-
-    return res
-
-def serialize_chain(chain):
-    chain_len = 4 + len(chain["ticker"]) + 1 + len(chain["name"]) + 1 + len(chain["shortName"]) + 1
-    return struct.pack("<HHI", CHAIN_MAGIC, chain_len, chain["id"]) + \
-        bytes(chain["ticker"], "ascii") + b'\0' + \
-        bytes(chain["name"], "ascii") + b'\0' + \
-        bytes(chain["shortName"], "ascii") + b'\0'
-
-def serialize_token(token):
-    addresses = serialize_addresses(token["addresses"])
-    token_len = 1 + len(addresses) + 1 + len(token["ticker"]) + 1
-    
-    return struct.pack("<HHB", ERC20_MAGIC, token_len, len(token["addresses"])) + \
-        addresses + \
-        struct.pack("B", token["decimals"]) + \
-        bytes(token["ticker"], "ascii") + b'\0'
 
 def pad_write(f, buf):
     f.write(buf)
@@ -73,45 +47,10 @@ def serialize_db(f, chains, tokens, version):
     if len(buf) > 0:
         pad_write(f, buf)
 
-def lookup_chain(chains_json, chain_id):
-    for chain in chains_json:
-        if chain["chainId"] == chain_id:
-            return chain
-    return None
-
-def process_token(tokens, chains, token_json, chains_json):
-    chain_id = token_json["chainId"]
-
-    chain = chains.get(chain_id)
-    if chain is None:
-        chain_json = lookup_chain(chains_json, chain_id)
-        chain = {
-            "id": chain_id,
-            "name": chain_json["name"],
-            "shortName": chain_json["shortName"],
-            "ticker": chain_json["nativeCurrency"]["symbol"],
-            "decimals": chain_json["nativeCurrency"]["decimals"],
-        }
-        
-        chains[chain_id] = chain
-
-    symbol = token_json["symbol"]
-    token = tokens.get(symbol)
-    
-    if token is None:
-        token = {
-            "addresses": {},
-            "ticker": symbol,
-            "decimals": token_json["decimals"]
-        }
-        tokens[symbol] = token
-
-    token["addresses"][chain_id] = token_json["address"]
-
 def main():
     def_version =  datetime.now().strftime("%Y%m%d")
 
-    parser = argparse.ArgumentParser(description='Create a database from a token and chain list')
+    parser = argparse.ArgumentParser(description='Create a database from a token, chain and ABI list')
     parser.add_argument('-t', '--token-list', help="the token list json", default="https://gateway.ipfs.io/ipns/tokens.uniswap.org")
     parser.add_argument('-c', '--chain-list', help="the chain list json", default="https://chainid.network/chains.json")
     parser.add_argument('-v', '--version', help="the version in YYYYMMDD format", default=def_version, type=int)
---
+++ tools/tokens.py
@@ -0,0 +1,64 @@
+import struct
+
+CHAIN_MAGIC = 0x4348
+ERC20_MAGIC = 0x3020
+
+def serialize_addresses(addresses):
+    res = b''
+    for id, address in addresses.items():
+        if len(address) != 42:
+            assert "Unexpected address format"
+        res = res + struct.pack("<I20s", id, bytes.fromhex(address[2:]))
+
+    return res
+
+def serialize_chain(chain):
+    chain_len = 4 + len(chain["ticker"]) + 1 + len(chain["name"]) + 1 + len(chain["shortName"]) + 1
+    return struct.pack("<HHI", CHAIN_MAGIC, chain_len, chain["id"]) + \
+        bytes(chain["ticker"], "ascii") + b'\0' + \
+        bytes(chain["name"], "ascii") + b'\0' + \
+        bytes(chain["shortName"], "ascii") + b'\0'
+
+def serialize_token(token):
+    addresses = serialize_addresses(token["addresses"])
+    token_len = 1 + len(addresses) + 1 + len(token["ticker"]) + 1
+    
+    return struct.pack("<HHB", ERC20_MAGIC, token_len, len(token["addresses"])) + \
+        addresses + \
+        struct.pack("B", token["decimals"]) + \
+        bytes(token["ticker"], "ascii") + b'\0'
+
+def lookup_chain(chains_json, chain_id):
+    for chain in chains_json:
+        if chain["chainId"] == chain_id:
+            return chain
+    return None
+
+def process_token(tokens, chains, token_json, chains_json):
+    chain_id = token_json["chainId"]
+
+    chain = chains.get(chain_id)
+    if chain is None:
+        chain_json = lookup_chain(chains_json, chain_id)
+        chain = {
+            "id": chain_id,
+            "name": chain_json["name"],
+            "shortName": chain_json["shortName"],
+            "ticker": chain_json["nativeCurrency"]["symbol"],
+            "decimals": chain_json["nativeCurrency"]["decimals"],
+        }
+        
+        chains[chain_id] = chain
+
+    symbol = token_json["symbol"]
+    token = tokens.get(symbol)
+    
+    if token is None:
+        token = {
+            "addresses": {},
+            "ticker": symbol,
+            "decimals": token_json["decimals"]
+        }
+        tokens[symbol] = token
+
+    token["addresses"][chain_id] = token_json["address"]
