132c82b9645b962489b444d884fc5da1a95359d6
Michele Balistreri <michele@bitgamma.com>
2025-05-14 12:42:21 +0200

    more db tool refactoring

---
+++ tools/abi.py
@@ -0,0 +1,9 @@
+import struct
+
+ABI_MAGIC = 0x4142
+
+def serialize_abi(abi):
+    return []
+
+def process_abi(abis, abi):
+    return {}
--- tools/shell-db.py
+++ tools/shell-db.py
@@ -4,9 +4,11 @@ from datetime import datetime
 import argparse
 import struct
 import requests
+import json
 
 from common import PAGE_SIZE, WORD_SIZE
 from tokens import *
+from abi import *
 
 VERSION_MAGIC = 0x4532
 
@@ -25,27 +27,37 @@ def pad_write(f, buf):
         f.write(0xff.to_bytes(1))
         size = size + 1
 
-def serialize_db(f, chains, tokens, version):
+def db_write(f, buf, entry):
+    if len(buf) + len(entry) <= PAGE_SIZE:
+        return buf + entry
+    else:
+        pad_write(f, buf)
+        return entry
+
+def serialize_db(f, chains, tokens, abis, version):
     buf = struct.pack("<HHI", VERSION_MAGIC, 4, version)
 
     for chain in chains.values():
         serialized_chain = serialize_chain(chain)
-        if len(buf) + len(serialized_chain) <= PAGE_SIZE:
-            buf = buf + serialized_chain
-        else:
-            pad_write(f, buf)
-            buf = serialized_chain
+        buf = db_write(f, buf, serialized_chain)
 
     for token in tokens.values():
         serialized_token = serialize_token(token)
-        if len(buf) + len(serialized_token) <= PAGE_SIZE:
-            buf = buf + serialized_token
-        else:
-            pad_write(f, buf)
-            buf = serialized_token
-    
+        buf = db_write(f, buf, serialized_token)
+
+    for abi in abis.values():
+        serialized_abi = serialize_abi(abi)
+        buf = db_write(f, buf, serialized_abi)
+        
     if len(buf) > 0:
         pad_write(f, buf)
+
+def read_json(url_or_path):
+    if url_or_path.startswith('http'):
+        return requests.get(url_or_path).json
+    else:
+        with open(url_or_path) as f:
+            return json.load(f)
 
 def main():
     def_version =  datetime.now().strftime("%Y%m%d")
@@ -53,22 +65,29 @@ def main():
     parser = argparse.ArgumentParser(description='Create a database from a token, chain and ABI list')
     parser.add_argument('-t', '--token-list', help="the token list json", default="https://gateway.ipfs.io/ipns/tokens.uniswap.org")
     parser.add_argument('-c', '--chain-list', help="the chain list json", default="https://chainid.network/chains.json")
+    parser.add_argument('-a', '--abi-list', help="the ABI json")
     parser.add_argument('-v', '--version', help="the version in YYYYMMDD format", default=def_version, type=int)
     parser.add_argument('-o', '--output', help="the output file")
     args = parser.parse_args()
 
-    token_list = requests.get(args.token_list).json()
-    chain_list = requests.get(args.chain_list).json()
+    token_list = read_json(args.token_list)
+    chain_list = read_json(args.chain_list)
+    abi_list = read_json(args.abi_list)
+
     version = args.version
 
     tokens = {}
     chains = {}
+    abis = {}
 
     for token in token_list["tokens"]:
         process_token(tokens, chains, token, chain_list)
     
+    for abi in abi_list:
+        process_abi(abis, abi)
+    
     with open(args.output, 'wb') as f:
-        serialize_db(f, chains, tokens, version)
+        serialize_db(f, chains, tokens, abis, version)
 
 if __name__ == "__main__":
     main()
