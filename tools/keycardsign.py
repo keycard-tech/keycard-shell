fd7dfc7f3f24d8a5f00740cefeb09b2a692816bd
Michele Balistreri <michele@bitgamma.com>
2025-08-25 08:26:41 +0200

    update keycard-py

--- tools/keycardsign.py
+++ tools/keycardsign.py
@@ -1,5 +1,4 @@
 import tkinter.simpledialog
-from secp256k1Crypto import PrivateKey
 
 def get_pin():
     return tkinter.simpledialog.askstring("Keycard PIN", "Enter PIN:", show='*')
@@ -14,7 +13,6 @@ def keycard_sign(digest):
         card.select()
         pairing_index, pairing_key = card.pair("KeycardDefaultPairing")
         card.open_secure_channel(pairing_index, pairing_key)
-        card.mutually_authenticate()
 
         pin = get_pin()
 
@@ -23,7 +21,6 @@ def keycard_sign(digest):
         
         try:
             sig = card.sign_with_path(digest, "m/43'/60'/1581'/35'/0")
-            ecdsa = PrivateKey()
-            return ecdsa.ecdsa_serialize_compact(ecdsa.ecdsa_deserialize(sig.signature))
+            return sig.signature
         finally:
             card.unpair(pairing_index)
